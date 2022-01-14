a3aa64617909c9d755d8480c2da49f14ee117532
Nikolai Baklanov <nikolaiko@mail.ru>
2022-01-14 13:46:12 +0600

    macOS check

--- Sources/iOSDevPackage/Services/Camera/Model/Photo.swift
+++ Sources/iOSDevPackage/Services/Camera/Model/Photo.swift
@@ -1,32 +1,34 @@
-import UIKit
+#if !os(macOS)
+    import UIKit
 
-@available(iOS 13.0, *)
-public struct Photo: Identifiable, Equatable {
-    public var id: String
-    public var originalData: Data
-    public var uniformTypeIdentifier: String?
-    
-    public init(id: String = UUID().uuidString, originalData: Data, uniformTypeIdentifier: String?) {
-        self.id = id
-        self.originalData = originalData
-        self.uniformTypeIdentifier = uniformTypeIdentifier
+    @available(iOS 13.0, *)
+    public struct Photo: Identifiable, Equatable {
+        public var id: String
+        public var originalData: Data
+        public var uniformTypeIdentifier: String?
+        
+        public init(id: String = UUID().uuidString, originalData: Data, uniformTypeIdentifier: String?) {
+            self.id = id
+            self.originalData = originalData
+            self.uniformTypeIdentifier = uniformTypeIdentifier
+        }
+        
+        public var compressedData: Data? {
+            UIImage(data: originalData)?.resize(targetWidth: 800).jpegData(compressionQuality: 0.5)
+        }
+        
+        public var thumbnailData: Data? {
+            UIImage(data: originalData)?.resize(targetWidth: 100).jpegData(compressionQuality: 0.5)
+        }
+        
+        public var thumbnailImage: UIImage? {
+            guard let data = thumbnailData else { return nil }
+            return UIImage(data: data)
+        }
+        
+        public var image: UIImage? {
+            guard let data = compressedData else { return nil }
+            return UIImage(data: data)
+        }
     }
-    
-    public var compressedData: Data? {
-        UIImage(data: originalData)?.resize(targetWidth: 800).jpegData(compressionQuality: 0.5)
-    }
-    
-    public var thumbnailData: Data? {
-        UIImage(data: originalData)?.resize(targetWidth: 100).jpegData(compressionQuality: 0.5)
-    }
-    
-    public var thumbnailImage: UIImage? {
-        guard let data = thumbnailData else { return nil }
-        return UIImage(data: data)
-    }
-    
-    public var image: UIImage? {
-        guard let data = compressedData else { return nil }
-        return UIImage(data: data)
-    }
-}
+#endif
