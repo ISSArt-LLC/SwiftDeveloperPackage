72946fbbda96ca1860b371c6abc5a548c13f68d3
Nikolai Baklanov <nikolaiko@mail.ru>
2022-01-28 11:27:59 +0600

    Added watchOS.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "iOSDevPackage",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v13), .watchOS(.v5)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
