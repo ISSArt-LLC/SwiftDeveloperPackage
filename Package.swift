b469ec79c45cb97fcba8c45a64ca190368b44af8
Nikolai Baklanov <nikolaiko@mail.ru>
2022-01-28 12:26:29 +0600

    Up to watchOS 6.0.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "iOSDevPackage",
     platforms: [
-        .iOS(.v13), .watchOS(.v5)
+        .iOS(.v13), .watchOS(.v6)
     ],
     products: [
         .library(
