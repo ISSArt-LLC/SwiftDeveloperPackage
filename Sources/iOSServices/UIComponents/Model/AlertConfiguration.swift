f71df709701b5685effdfbcf196347b55e6f2b59
Nikolai Baklanov <nikolaiko@mail.ru>
2022-04-14 10:40:25 +0600

    Update initializer.

--- Sources/iOSServices/UIComponents/Model/AlertConfiguration.swift
+++ Sources/iOSServices/UIComponents/Model/AlertConfiguration.swift
@@ -4,14 +4,34 @@ import SwiftUI
 public struct AlertConfiguration {
     public var title: String
     public var message: String
-    public var placeholder: String = emptyString
-    public var accept: String = okLabel
-    public var cancel: String? = cancelLabel
-    public var secondaryActionTitle: String? = nil
-    public var keyboardType: UIKeyboardType = .default
-    public var action: (String?) -> Void
-    public var secondaryAction: VoidCallback? = nil
+    public var placeholder: String
+    public var accept: String
+    public var cancel: String?
+    public var secondaryActionTitle: String?
+    public var keyboardType: UIKeyboardType
+    public var action: NullableStringCallback
+    public var secondaryAction: VoidCallback?
     
-    public init() {}
+    public init(
+        alertTitle: String,
+        alertMessage: String,
+        mainAction: @escaping NullableStringCallback,
+        alertPlaceholder: String = emptyString,
+        acceptText: String = okLabel,
+        cancelText: String? = cancelLabel,
+        alterActionTitle: String? = nil,
+        alertKeyboardType: UIKeyboardType = .default,
+        alterAction: VoidCallback? = nil
+    ) {
+        title = alertTitle
+        message = alertMessage
+        placeholder = alertPlaceholder
+        accept = acceptText
+        cancel = cancelText
+        secondaryActionTitle = alterActionTitle
+        keyboardType = alertKeyboardType
+        action = mainAction
+        secondaryAction = alterAction
+    }
 }
 
--- Sources/iOSServices/UIComponents/Model/CallbackTypes.swift
+++ Sources/iOSServices/UIComponents/Model/CallbackTypes.swift
@@ -1,3 +1,4 @@
 import Foundation
 
 public typealias VoidCallback = () -> Void
+public typealias NullableStringCallback = (String?) -> Void
